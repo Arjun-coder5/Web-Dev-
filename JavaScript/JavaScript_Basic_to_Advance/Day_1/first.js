09e2513797b30772db3603ae463226afaea8164b
Arjun-coder5 <arjunsorout526@gmail.com>
2025-09-14 17:50:50 +0530

    Js

---
+++ JavaScript/JavaScript_Basic_to_Advance/Day_1/first.js
@@ -0,0 +1,14 @@
+console.log("Arjun");
+
+name = "Tony Stark";
+console.log(name);
+price = 99;
+console.log(price);
+
+x = null;
+
+
+// Three type of variable declaring :
+// 1. Var
+// 2. let
+// 3. const
