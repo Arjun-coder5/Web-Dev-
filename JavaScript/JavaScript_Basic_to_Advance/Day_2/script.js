808e246248a71e24a50e7beebbb949b3833e27a0
Arjun-coder5 <arjunsorout526@gmail.com>
2025-09-15 22:39:32 +0530

    Js Day 2

---
+++ JavaScript/JavaScript_Basic_to_Advance/Day_2/script.js
@@ -0,0 +1,120 @@
+//This code print hello world :
+/* This is a multi line comment */
+// console.log("Hello World");
+
+
+//Arithmatic Operator :
+
+// let a = 5;
+// let b = 2;
+// console.log("a + b = " + a+b);
+// console.log("a - b = " + a-b);
+// console.log("a * b = " + a*b);
+// console.log("a / b = " + a/b);
+// console.log("a % b = " + a%b);
+// console.log("a ** b = " + a**b);
+
+
+//Unary Operator :
+
+// console.log(a++);
+// console.log(a--);
+
+ 
+//Assignment Operator :
+// let c = 4;
+// let d = 7;
+
+// c += 4;
+// c -= 4;
+// c *= 4;
+// c /= 4;
+// c %= 4;
+// c **= 4;
+// console.log("c = ", c);
+
+// Comparison Operator :
+//  let e = 5;
+//  let f = "5";
+// console.log(e==f);
+// console.log(e!=f);
+// console.log(e!==f);
+// console.log(e===f);
+// console.log(e>f);
+// console.log(e<f);
+
+// Logical operators :
+// let g = 5;
+// let h = 5 ;
+// console.log(g&&h);
+// console.log(g||h);
+// console.log(!g);
+
+
+// Conditional Statement :
+
+// let age = 19;
+// if(age>=25){
+//   console.log("You can vote");
+  
+// }else{
+//   console.log("hat be chotu");
+  
+// }
+
+// Ternary Operator :
+// let age = 25;
+
+// console.log(age>18?"adult" : "Not Adult");
+
+let num = prompt("Enter a No :")
+if(num%5 === 0){
+  console.log("Num is multiple of 5");
+}else{
+  console.log('Not');
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
