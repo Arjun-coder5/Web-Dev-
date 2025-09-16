43e9d7e94027129448d15534947b4e22c794a39a
Arjun-coder5 <arjunsorout526@gmail.com>
2025-09-16 23:18:37 +0530

    Basic Calculator

---
+++ 50_Days_Project(HTML-CSS-JS)/Day_2_Calculator/script.js
@@ -0,0 +1,26 @@
+const buttonE1 = document.querySelectorAll("button");
+const inputFieldE1 = document.getElementById("result");
+for(let i = 0;i<buttonE1.length;i++){
+   buttonE1[i].addEventListener("click",()=>{
+    const buttonValue = (buttonE1[i].textContent);
+    if(buttonValue === "C"){
+      clearReasult()
+    }else if(buttonValue==="="){
+      calculateResult()
+    }else{
+      appendValue(buttonValue)
+    }
+   })
+}
+
+
+function clearReasult(){
+  inputFieldE1.value = "" ;
+}
+
+function calculateResult(){
+   inputFieldE1.value = eval(inputFieldE1.value);
+}
+function appendValue(buttonValue){
+  inputFieldE1.value += buttonValue;
+}
